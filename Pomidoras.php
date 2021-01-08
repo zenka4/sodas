602516abbb244ad1d138fd64e2f878eee104eac9
zenka4 <zenka4@inbox.lt>
2021-01-08 21:39:50 +0200

    -

--- Pomidoras.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Jeff;
-
-class Pomidoras extends ProsenelinisAugalas
-{
-}
--- cutSomething.php
+++
@@ -1,3 +0,0 @@
-<?php $kiekis = rand(2, 9) ?>
-
-<input type="hidden" name="kiekis[<?= $antiCovid->{'id'} ?>]" value="<?= $kiekis ?>">
